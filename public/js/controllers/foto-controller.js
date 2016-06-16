66f49c6197c92c1fecdbe432f4d51e2eb1d05859
mimica <jmimica@gmail.com>
2016-06-15 22:50:01 -0300

    Update class 11.

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,11 +1,10 @@
-angular.module('alurapic').controller('FotoController', function ($scope, recursoFoto, $routeParams) {
+angular.module('alurapic').controller('FotoController', function ($scope, recursoFoto, cadastroDeFotos, $routeParams) {
 	
 	$scope.foto = {};
 	$scope.mensagem = '';
 	console.log($routeParams.fotoId);
 
 	
-
 	if ($routeParams.fotoId)
 	{
 		recursoFoto.get({fotoId : $routeParams.fotoId}, function(foto) {
@@ -15,77 +14,22 @@ angular.module('alurapic').controller('FotoController', function ($scope, recurs
 			$scope.mensagem = 'Não foi possível obter a foto.';
 		});
 	}
-	/*
-	if ($routeParams.fotoId) {
-		$http.get('v1/fotos/'+$routeParams.fotoId)
-			.success(function(foto) {
-				$scope.foto = foto;
-			})
-			.error(function(error) {
-				$scope.mensagem = 'Não foi possível obter a foto com Id '+$routeParams.fotoId;
-			});
-	}
-	*/
 
 	$scope.submeter = function () {
+		
 		if ($scope.formulario.$valid)
 		{
-			if ($scope.foto._id)
-			{
-				recursoFoto.update({fotoId : $scope.foto._id}, $scope.foto, function() {
-					$scope.foto = {};
-						
-					//Veja que ao limparmos o formulário, automaticamente nossa validação é disparada. Contudo, se você quiser que isso não acontece, logo depois de limpar o formulário faça:
-					$scope.formulario.$setPristine();
+			cadastroDeFotos.cadastrar($scope.foto)
+				.then(function (dados) {
+					$scope.mensagem = dados.mensagem;
 
-					$scope.mensagem = 'Foto alterada com sucesso.';
-					console.log('Foto alterada com sucesso.');
-				}, function (error) {
-					$scope.mensagem = 'Não foi possível alterar a foto.';
-					console.log(error);
+					// limpa o formulário se for inclusão
+					if (dados.inclusao)
+						$scope.foto = {};
+				})
+				.catch(function(dados) {
+					$scope.mensagem = dados.mensagem;
 				});
-
-				/*
-				// Update
-				$http.put('v1/fotos/' + $scope.foto._id, $scope.foto)
-					.success(function () {
-						$scope.foto = {};
-						
-						//Veja que ao limparmos o formulário, automaticamente nossa validação é disparada. Contudo, se você quiser que isso não acontece, logo depois de limpar o formulário faça:
-						$scope.formulario.$setPristine();
-
-						$scope.mensagem = 'Foto alterada com sucesso.';
-						console.log('Foto alterada com sucesso.');
-					})
-					.error(function (error) {
-						$scope.mensagem = 'Não foi alterada a foto.';
-						console.log(error);
-					});
-				*/
-
-			}
-			else {
-				// insert
-
-				//recursoFoto.
-
-				/*
-				$http.post('v1/fotos', $scope.foto)
-					.success(function () {
-						$scope.foto = {};
-
-						//Veja que ao limparmos o formulário, automaticamente nossa validação é disparada. Contudo, se você quiser que isso não acontece, logo depois de limpar o formulário faça:
-						$scope.formulario.$setPristine();
-
-						$scope.mensagem = 'Foto incluída com sucesso.';
-						console.log('Foto cadastrada com sucesso.');
-					})
-					.error(function (error) {
-						$scope.mensagem = 'Não foi incluir a foto.';
-						console.log(error);
-					});
-				*/
-			}
 		}
 	};
 
--- public/js/services/meus-servicos.js
+++ public/js/services/meus-servicos.js
@@ -1,6 +1,7 @@
 angular.module('meusServicos', ['ngResource'])
 .factory('recursoFoto', function($resource) {
 
+	// Return an JS object
 	return $resource('/v1/fotos/:fotoId', null, {
 		update : {
 			method: 'PUT'
@@ -8,4 +9,44 @@ angular.module('meusServicos', ['ngResource'])
 	});
 
 
+})
+.factory('cadastroDeFotos', function(recursoFoto, $q) {
+
+	var servico = {};
+
+	servico.cadastrar = function(foto) {
+
+		return $q(function(resolve, reject) {
+			if (foto._id) {
+				recursoFoto.update({fotoId: foto._id}, foto, function() {
+					resolve({
+						mensagem : 'Foto ' + foto.titulo + ' atualizada com sucesso!',
+						inclusao : false
+					});
+				}, function(error) {
+					console.log(error);
+					reject({
+						mensagem : 'Não foi possível alterar a foto ' + foto.titulo
+					});
+				});
+			}
+			else {
+				recursoFoto.save(foto, function() {
+					resolve({
+						mensagem : 'Foto ' + foto.titulo + ' incluída com sucesso!',
+						inclusao : true
+					});
+				}, function(error) {
+					console.log(error);
+					reject({
+						mensagem : 'Não foi possível salvar a foto ' + foto.titulo
+					});
+				});
+			}
+		});
+
+	};
+
+	// Return an JS object
+	return servico;
 });
