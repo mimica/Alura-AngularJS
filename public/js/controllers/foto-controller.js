e7181f73e393f1fdf50099c613ece78f748a30d2
mimica <jmimica@gmail.com>
2016-06-15 22:16:09 -0300

    Update for class 10.

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,9 +1,21 @@
-angular.module('alurapic').controller('FotoController', function ($scope, $http, $routeParams) {
+angular.module('alurapic').controller('FotoController', function ($scope, recursoFoto, $routeParams) {
 	
 	$scope.foto = {};
 	$scope.mensagem = '';
 	console.log($routeParams.fotoId);
 
+	
+
+	if ($routeParams.fotoId)
+	{
+		recursoFoto.get({fotoId : $routeParams.fotoId}, function(foto) {
+			$scope.foto = foto;
+		}, function (error) {
+			console.log(error);
+			$scope.mensagem = 'Não foi possível obter a foto.';
+		});
+	}
+	/*
 	if ($routeParams.fotoId) {
 		$http.get('v1/fotos/'+$routeParams.fotoId)
 			.success(function(foto) {
@@ -13,11 +25,27 @@ angular.module('alurapic').controller('FotoController', function ($scope, $http,
 				$scope.mensagem = 'Não foi possível obter a foto com Id '+$routeParams.fotoId;
 			});
 	}
+	*/
 
 	$scope.submeter = function () {
 		if ($scope.formulario.$valid)
 		{
-			if ($scope.foto._id) {
+			if ($scope.foto._id)
+			{
+				recursoFoto.update({fotoId : $scope.foto._id}, $scope.foto, function() {
+					$scope.foto = {};
+						
+					//Veja que ao limparmos o formulário, automaticamente nossa validação é disparada. Contudo, se você quiser que isso não acontece, logo depois de limpar o formulário faça:
+					$scope.formulario.$setPristine();
+
+					$scope.mensagem = 'Foto alterada com sucesso.';
+					console.log('Foto alterada com sucesso.');
+				}, function (error) {
+					$scope.mensagem = 'Não foi possível alterar a foto.';
+					console.log(error);
+				});
+
+				/*
 				// Update
 				$http.put('v1/fotos/' + $scope.foto._id, $scope.foto)
 					.success(function () {
@@ -33,16 +61,22 @@ angular.module('alurapic').controller('FotoController', function ($scope, $http,
 						$scope.mensagem = 'Não foi alterada a foto.';
 						console.log(error);
 					});
+				*/
+
 			}
 			else {
 				// insert
+
+				//recursoFoto.
+
+				/*
 				$http.post('v1/fotos', $scope.foto)
 					.success(function () {
 						$scope.foto = {};
 
 						//Veja que ao limparmos o formulário, automaticamente nossa validação é disparada. Contudo, se você quiser que isso não acontece, logo depois de limpar o formulário faça:
 						$scope.formulario.$setPristine();
-						
+
 						$scope.mensagem = 'Foto incluída com sucesso.';
 						console.log('Foto cadastrada com sucesso.');
 					})
@@ -50,6 +84,7 @@ angular.module('alurapic').controller('FotoController', function ($scope, $http,
 						$scope.mensagem = 'Não foi incluir a foto.';
 						console.log(error);
 					});
+				*/
 			}
 		}
 	};
--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,10 +1,18 @@
 
-angular.module('alurapic').controller('FotosController', function($scope, $http) {
+angular.module('alurapic').controller('FotosController', function($scope, recursoFoto) {
 
 	$scope.fotos = [];
 	$scope.filtro = '';
 	$scope.mensagem = '';
 	
+	recursoFoto.query(function(fotos) {
+		$scope.fotos = fotos;
+	}, function (error) {
+		console.log(error);
+	});
+
+
+	/*
 	$http.get('v1/fotos')
 		.success(function (result) {
 			$scope.fotos = result;
@@ -12,25 +20,23 @@ angular.module('alurapic').controller('FotosController', function($scope, $http)
 		.error(function (error) {
 			console.log("Error retriving data:"+error);
 		});
+		*/
+
+	
 
 	$scope.remover = function (foto) {
+
 		// Remove a foto
 		console.log(foto);
-		$http.delete('v1/fotos/'+foto._id)
-			.success(function() {
-				console.log('Foto ' + foto.titulo + ' for removida com sucesso.');
 
-				// Find the foto index
-				var indiceFoto = $scope.fotos.indexOf(foto);
-				$scope.fotos.splice(indiceFoto, 1);
-
-				$scope.mensagem = 'Foto ' + foto.titulo + ' for removida com sucesso.';
-			})
-			.error(function(error) {
-				console.log(error);
-				console.log('Não foi possível remover a foto ' + foto.titulo + '.');
-
-				$scope.mensagem = 'Não foi possível remover a foto ' + foto.titulo + '.';
-			});
+		recursoFoto.delete({fotoId : foto._id}, function() {
+			// Find the foto index
+			var indiceFoto = $scope.fotos.indexOf(foto);
+			$scope.fotos.splice(indiceFoto, 1);
+			$scope.mensagem = 'Foto ' + foto.titulo + ' for removida com sucesso.';
+		}, function (error) {
+			console.log(error);
+			$scope.mensagem = 'Não foi possível remover a foto ' + foto.titulo + '.';
+		});		
 	};
 });
